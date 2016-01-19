08264cbe01de6096c1d5580d7eb8d2671fb3c4b2
Julian Berman <Julian@GrayVines.com>
2016-01-19 17:28:16 -0500

    More scaffolding.

--- src-cljs/githubcomments/core.cljs
+++ src-cljs/githubcomments/core.cljs
@@ -2,7 +2,12 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
    (:require [cljs.core.async :refer [<!]]
              [goog.dom :as dom]
-             [githubcomments.github :refer [repo-comments]]))
+             [githubcomments.github :refer [repo-comments
+                                            render-user-content-markdown]]))
 
-(go (let [response (<! (repo-comments "Magnetic" "Platform-Guild"))]
-      (.log js/console (first (map :body (:body response))))))
+(def repo ["Magnetic" "Platform-Guild"])
+
+(defn render-comment [comment] (.log js/console (:body comment)))
+
+(go (let [response (<! (repo-comments repo))]
+      (dorun (map render-comment (:body response)))))
--- src-cljs/githubcomments/github.cljs
+++ src-cljs/githubcomments/github.cljs
@@ -5,8 +5,20 @@
 (def url (Uri. "https://api.github.com/"))
 (defn api-call
   ([endpoint] (api-call http/get endpoint))
-  ([method endpoint] (method (.resolve url endpoint)
-                             {:with-credentials? false})))
+  ([method endpoint & [parameters]] (method (.resolve url endpoint)
+                                            {:with-credentials? false
+                                             :json-parameters parameters})))
 
-(defn repo-comments [owner name]
+(defn repo-comments [[owner name]]
   (api-call (Uri. (str "/repos/" owner "/" name "/comments"))))
+
+(defn render-markdown [markdown mode context]
+  (api-call http/post (Uri. "/markdown") {:text markdown
+                                          :mode mode
+                                          :context context}))
+
+(defn render-plain-markdown [markdown]
+  (render-markdown markdown "markdown" nil))
+
+(defn render-user-content-markdown [markdown [repo-name repo-owner]]
+  (render-markdown markdown "gfm" (str repo-owner "/" repo-name)))
