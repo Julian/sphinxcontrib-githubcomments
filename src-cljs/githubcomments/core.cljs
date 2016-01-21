81f904b618c071ceac38304ad069015f7e4b699e
Julian Berman <Julian@GrayVines.com>
2016-01-21 14:34:16 -0500

    More!

--- src-cljs/githubcomments/core.cljs
+++ src-cljs/githubcomments/core.cljs
@@ -1,15 +1,15 @@
 (ns githubcomments.core
   (:require-macros [cljs.core.async.macros :refer [go]])
-   (:require [cljs.core.async :as async]
-             [goog.dom :as dom]
-             [githubcomments.github :refer [repo-comments
-                                            render-user-content-markdown]]))
-
-(def repo ["Magnetic" "Platform-Guild"])
+  (:require [cljs.core.async :as async]
+            [goog.dom :as dom]
+            [goog.dom.dataset :as dataset]
+            [githubcomments.github :refer [repo-comments
+                                           render-user-content-markdown]]))
 
 (defn rendered-comment [repo comment]
   "Render the given comment in the context of the given repository."
-  (go (let [response (async/<! (render-user-content-markdown (:body comment) repo))]
+  (go (let [response (async/<!
+                       (render-user-content-markdown (:body comment) repo))]
         (.log js/console (:body response)))))
 
 (enable-console-print!)
@@ -18,8 +18,10 @@
   (map (partial rendered-comment repo) comments))
 
 (defn init []
-  (go (let [response (async/<! (repo-comments repo))
-            element (dom/getElement "github-comments")]
+  (go (let [element (dom/getElement "github-comments")
+            repo [(dataset/get element "repositoryOwner")
+                  (dataset/get element "repositoryName")]
+            response (async/<! (repo-comments repo))]
         (apply println (rendered-comments repo (:body response))))))
 
 (init)
