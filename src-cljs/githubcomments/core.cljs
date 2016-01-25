e6120bb62ee99b36072c317edc7b1180f9af05a3
Julian Berman <Julian@GrayVines.com>
2016-01-24 19:57:36 -0500

    Tidying.

--- src-cljs/githubcomments/core.cljs
+++ src-cljs/githubcomments/core.cljs
@@ -14,15 +14,6 @@
                      (take 10 comments))
              "</ul>")))
 
-(defn rendered-comment [repo comment]
-  "Render the given comment in the context of the given repository."
-  (go (let [response (async/<!
-                       (render-user-content-markdown (:body comment) repo))]
-        (:body response))))
-
-(defn rendered-comments [repo comments]
-  (map :body_html comments))
-
 (defn init []
   (go (let [element (dom/getElement "github-comments")
             repo [(dataset/get element "repositoryOwner")
@@ -30,7 +21,7 @@
             path (dataset/get element "path")
             response (async/<! (repo-comments repo "html"))
             relevant (filter #(= (:path %1) path) (:body response))
-            comments (rendered-comments repo relevant)]
-        (display-comments comments element))))
+            comments-html (map :body_html relevant)]
+        (display-comments comments-html element))))
 
 (init)
