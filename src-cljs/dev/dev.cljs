ed744e07dd387bfe9510afd57440a53ec4145384
Julian Berman <Julian@GrayVines.com>
2016-02-12 14:33:58 -0500

    I don't know how to pass these in yet, but this looks like a thing.

--- src-cljs/dev/dev.cljs
+++ src-cljs/dev/dev.cljs
@@ -1,2 +1,7 @@
-(ns githubcomments.dev)
+(ns githubcomments.dev
+  (:require [clojure.browser.repl :as repl]))
+
 (enable-console-print!)
+
+(goog-define repl-uri false)
+(if repl-uri (defonce conn (repl/connect repl-uri)))
