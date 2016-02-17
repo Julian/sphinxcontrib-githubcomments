8f9ba07dedb8a5f6aa25c25dd1ced5d526fe8abe
Julian Berman <Julian@GrayVines.com>
2016-02-17 11:03:55 -0500

    Start trying to get optimizations and other things to work for releases.

--- project.clj
+++ project.clj
@@ -12,13 +12,26 @@
                  [cljs-http "0.1.39"]
                  [cljsjs/moment "2.10.6-2"]
                  [hiccups "0.3.0"]]
-  :plugins [[lein-cljsbuild "1.1.2"]]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-npm "0.6.1"]]
   :source-paths ["src-cljs/main/"]
   :hooks [leiningen.cljsbuild]
-  :cljsbuild {:builds [{:compiler {:output-to "sphinxcontrib/githubcomments/_static/githubcomments.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]}
-  :profiles {:dev {:source-paths ["src-cljs/main/" "src-cljs/dev/"]
-                   :dependencies [[com.cemerick/piggieback  "0.2.1"]]
+  :cljsbuild {:builds
+              {:main
+               {:compiler {:output-to "sphinxcontrib/githubcomments/_static/githubcomments.js"
+                           :output-dir "sphinxcontrib/githubcomments/_static/"
+                           :optimizations :none}}}}
+  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                                  [com.cemerick/piggieback  "0.2.1"]]
+                   :source-paths ["src-cljs/main/" "src-cljs/dev/"]
                    :repl-options  {:nrepl-middleware
-                                   [cemerick.piggieback/wrap-cljs-repl]}}})
+                                   [cemerick.piggieback/wrap-cljs-repl]}
+
+                   :cljsbuild {:builds
+                               {:main
+                                {:compiler {:pretty-print true}
+                                 :source-map true}}}}
+             :release {:cljsbuild {:builds
+                               {:main
+                                {:compiler {:optimizations :advanced
+                                            :pretty-print false}}}}}})
