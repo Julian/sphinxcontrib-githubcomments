472f45134990a373a50b05167b7cb4a4ecdfcacc
Julian Berman <Julian@GrayVines.com>
2016-02-17 09:54:43 -0500

    Add hooks support which hooks into some stuff I don't use yet.

--- project.clj
+++ project.clj
@@ -7,17 +7,18 @@
          :out)))
 
 (defproject sphinxcontrib/githubcomments version
-  :source-paths ["src-cljs/main/"]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [cljs-http "0.1.39"]
                  [cljsjs/moment "2.10.6-2"]
                  [hiccups "0.3.0"]]
+  :plugins [[lein-cljsbuild "1.1.2"]]
+  :source-paths ["src-cljs/main/"]
+  :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:compiler {:output-to "sphinxcontrib/githubcomments/_static/githubcomments.js"
                                    :optimizations :whitespace
                                    :pretty-print true}}]}
   :profiles {:dev {:source-paths ["src-cljs/main/" "src-cljs/dev/"]
-                   :plugins [[lein-cljsbuild "1.1.2"]]
                    :dependencies [[com.cemerick/piggieback  "0.2.1"]]
                    :repl-options  {:nrepl-middleware
                                    [cemerick.piggieback/wrap-cljs-repl]}}})
